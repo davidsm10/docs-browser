b40caf1ef0b9ea99f2055d48bd92f01cb91c12b4
davidsm10 <davidsm10@systemli.org>
2026-08-15 10:08:57 -0400

    feat: allow documents to be html or any other text formats
    also move extracting title into separated functions

--- src/content.ts
+++ src/content.ts
@@ -21,26 +21,38 @@ export async function saveContent() {
   const textDecoder = new TextDecoder();
   const entriesList: List = [];
   for (const entry of entries) {
-    if (entry.header.type === "file" && entry.data) {
-      const markdown = textDecoder.decode(entry.data);
-      const titleStart = markdown.indexOf("# ");
-      const titleEnd = markdown.indexOf("\n", titleStart);
-      const title = markdown.substring(titleStart + 2, titleEnd);
-      let html = md.render(markdown);
-      const dom = new DOMParser().parseFromString(html, "text/html");
-      const tables = dom.querySelectorAll("table");
-      for (const table of tables) {
-        const clone = table.cloneNode(true);
-        const wrapper = dom.createElement("div");
-        wrapper.className = "table-wrapper";
-        wrapper.appendChild(clone);
-        table.replaceWith(wrapper);
-      }
-      html = dom.body.innerHTML;
-      const path = "/" + entry.header.name;
-      entriesList.push({ path, title });
-      await localforage.setItem(path, html);
+    if (entry.header.type !== "file" || !entry.data) {
+      continue;
     }
+
+    const text = textDecoder.decode(entry.data);
+
+    let html: string;
+    if (entry.header.name.endsWith(".html")) {
+      html = text;
+    } else if (entry.header.name.endsWith(".md")) {
+      html = md.render(text);
+    } else {
+      html = md.render(text);
+    }
+
+    const dom = new DOMParser().parseFromString(html, "text/html");
+    let title = getTitleFromDom(dom);
+    if (!title) {
+      title = getTitleFromPath(entry.header.name);
+    }
+    const tables = dom.querySelectorAll("table");
+    for (const table of tables) {
+      const clone = table.cloneNode(true);
+      const wrapper = dom.createElement("div");
+      wrapper.className = "table-wrapper";
+      wrapper.appendChild(clone);
+      table.replaceWith(wrapper);
+    }
+    html = dom.body.innerHTML;
+    const path = "/" + entry.header.name;
+    entriesList.push({ path, title });
+    await localforage.setItem(path, html);
   }
 
   const tree = listToTree(entriesList);
@@ -82,4 +94,25 @@ function listToTree(arr: List) {
   });
 
   return tree;
+}
+
+function getTitleFromDom(dom: Document) {
+  const titleElem = dom.getElementsByTagName("title")[0];
+  const firstH1Elem = dom.getElementsByTagName("h1")[0];
+
+  return (
+    (titleElem && titleElem.textContent) ||
+    (firstH1Elem && firstH1Elem.textContent) ||
+    null
+  );
+}
+
+function getTitleFromPath(path: string) {
+  const pathParts = path.split("/");
+  const lastPart = pathParts[pathParts.length - 1];
+  if (lastPart.startsWith("index.")) {
+    return pathParts[pathParts.length - 2];
+  } else {
+    return lastPart;
+  }
 }
