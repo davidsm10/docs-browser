bc36f8a2ebd81df1514689d8e8ad3935f6235fbf
davidsm10 <davidsm10@systemli.org>
2026-08-16 13:20:14 -0400

    feat: improve setup view
    - Adjust wording
    - Aling in the middle
    - Show error to user

--- src/components/setupView/setupView.tsx
+++ src/components/setupView/setupView.tsx
@@ -1,14 +1,33 @@
-import { useEffect } from "preact/hooks";
+import { useEffect, useState } from "preact/hooks";
 import "./setupView.css";
 import { saveContent } from "../../content";
 
 export default function SetupView(props: { onSetupDone: () => void }) {
+  const [error, setError] = useState<string | null>(null);
+
   useEffect(() => {
     const setup = async () => {
-      await saveContent();
-      props.onSetupDone();
+      try {
+        await saveContent();
+        props.onSetupDone();
+      } catch (err) {
+        setError(String(err));
+      }
     };
     setup();
   }, []);
-  return <div>Decompressing, converting and saving docs</div>;
+  return (
+    <div className="setup-view">
+      <div>
+        <h1>Initial setup in progress</h1>
+        <p>This is a one time setup</p>
+        {error && (
+          <p>
+            <span class="error">Error: </span>
+            {error}
+          </p>
+        )}
+      </div>
+    </div>
+  );
 }
